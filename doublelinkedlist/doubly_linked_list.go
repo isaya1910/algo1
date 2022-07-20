4eb0064a8798058e0d6dd7ad007ba9c5680bf1ee
isaya1910 <ibaigereyev@klika-tech.com>
2022-07-20 23:12:50 +0600

    doubly linked list

---
+++ doublelinkedlist/doubly_linked_list.go
@@ -0,0 +1,137 @@
+package doublelinkedlist
+
+import (
+	"errors"
+)
+
+type Node struct {
+	prev  *Node
+	next  *Node
+	value int
+}
+
+type LinkedList2 struct {
+	head *Node
+	tail *Node
+}
+
+func (l *LinkedList2) AddInTail(item Node) {
+	if l.head == nil {
+		l.head = &item
+		l.head.next = nil
+		l.head.prev = nil
+	} else {
+		l.tail.next = &item
+		item.prev = l.tail
+	}
+
+	l.tail = &item
+	l.tail.next = nil
+}
+
+func (l *LinkedList2) Count() int {
+	count := 0
+	iteratorNode := l.head
+	for iteratorNode != nil {
+		count++
+		iteratorNode = iteratorNode.next
+	}
+	return count
+}
+
+func (l *LinkedList2) Find(n int) (Node, error) {
+	iteratorNode := l.head
+	for iteratorNode != nil {
+		if iteratorNode.value == n {
+			return *iteratorNode, nil
+		}
+		iteratorNode = iteratorNode.next
+	}
+	return Node{nil, nil, -1}, errors.New("node not found")
+}
+
+func (l *LinkedList2) FindAll(n int) []Node {
+	var nodes []Node
+	iteratorNode := l.head
+	for iteratorNode != nil {
+		if iteratorNode.value == n {
+			nodes = append(nodes, *iteratorNode)
+		}
+		iteratorNode = iteratorNode.next
+	}
+	return nodes
+}
+
+func (l *LinkedList2) Delete(n int, all bool) {
+	current := l.head
+	isFind := false
+	for current != nil {
+		if isFind && !all {
+			return
+		}
+		if current.value == n && current == l.head {
+			current = current.next
+			isFind = true
+			l.head = current
+			if current == nil {
+				l.tail = l.head
+				return
+			}
+			if current.next == nil {
+				l.tail = current
+				return
+			}
+			continue
+		}
+		if current.value == n {
+			temp := current
+			current = current.next
+			temp.prev.next = current
+			isFind = true
+			if current == nil {
+				l.tail = temp.prev
+				return
+			}
+			current.prev = temp.prev
+			if current.next == nil {
+				l.tail = current
+				return
+			}
+			continue
+		}
+		if current.value != n {
+			current = current.next
+		}
+	}
+}
+
+func (l *LinkedList2) Insert(after *Node, add Node) {
+	temp := after.next
+	after.next = &add
+	add.prev = after
+	(&add).next = temp
+	if temp == nil {
+		l.tail = &add
+		return
+	}
+	temp.prev = &add
+}
+
+func (l *LinkedList2) InsertFirst(first Node) {
+	temp := l.head
+	l.head = &first
+	(&first).next = temp
+	if temp == nil {
+		l.tail = l.head
+		return
+	}
+	temp.prev = &first
+	if temp.next == nil {
+		l.tail = temp
+	}
+}
+
+func (l *LinkedList2) Clean() {
+	l.head = nil
+	l.tail = nil
+}
---
+++ doublelinkedlist/doubly_linked_list_test.go
@@ -0,0 +1,100 @@
+package doublelinkedlist
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	testObject := LinkedList2{}
+	head := Node{next: nil, value: 1}
+	testObject.AddInTail(head)
+	assert.NotNil(t, testObject.head)
+	assert.NotNil(t, testObject.tail)
+	assert.Equal(t, testObject.head.value, 1)
+	newNode := Node{next: nil, value: 2}
+	testObject.Insert(testObject.head, newNode)
+	assert.Equal(t, testObject.Count(), 2)
+	node := testObject.head.next
+	assert.NotNil(t, node)
+	testObject.Insert(testObject.head, Node{nil, nil, 5})
+	assert.Equal(t, testObject.Count(), 3)
+	assert.Equal(t, testObject.head.next.value, 5)
+	assert.Equal(t, testObject.head.next.next.value, 2)
+}
+
+func TestInsertFirst(t *testing.T) {
+	testObject := LinkedList2{}
+	testObject.InsertFirst(Node{nil, nil, 1})
+	assert.Equal(t, testObject.head.value, 1)
+	testObject.InsertFirst(Node{nil, nil, 2})
+	assert.Equal(t, testObject.head.value, 2)
+	assert.Equal(t, testObject.head.next.value, 1)
+}
+
+func TestDelete(t *testing.T) {
+	testObject := LinkedList2{}
+	testObject.AddInTail(Node{nil, nil, 1})
+
+	testObject.AddInTail(Node{nil, nil, 2})
+	testObject.AddInTail(Node{nil, nil, 3})
+
+	testObject.InsertFirst(Node{nil, nil, 2})
+	testObject.InsertFirst(Node{nil, nil, 2})
+	testObject.InsertFirst(Node{nil, nil, 2})
+
+	assert.Equal(t, testObject.Count(), 6)
+	assert.Equal(t, testObject.head.value, 2)
+
+	testObject.Delete(2, false)
+	assert.Equal(t, testObject.Count(), 5)
+	testObject.Delete(2, true)
+	assert.Equal(t, testObject.Count(), 2)
+
+	testObject.Delete(3, true)
+	assert.Equal(t, testObject.Count(), 1)
+	assert.Equal(t, testObject.head.value, 1)
+	testObject.Delete(1, true)
+	assert.Equal(t, testObject.Count(), 0)
+	assert.Nil(t, testObject.head)
+	assert.Nil(t, testObject.tail)
+}
+
+func TestDelete2(t *testing.T) {
+	testObject := LinkedList2{}
+
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 2})
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 4})
+
+	assert.Equal(t, 8, testObject.Count())
+	testObject.Delete(1, true)
+
+	assert.Equal(t, testObject.Count(), 2)
+	assert.Equal(t, testObject.head.value, 2)
+	assert.Equal(t, testObject.tail.value, 4)
+}
+
+func TestFind(t *testing.T) {
+	testObject := LinkedList2{}
+	testObject.AddInTail(Node{nil, nil, 1})
+	testObject.AddInTail(Node{nil, nil, 5})
+	actual, e := testObject.Find(1)
+	assert.Nil(t, e)
+	assert.Equal(t, actual.value, 1)
+
+	actual, e = testObject.Find(2)
+	assert.NotNil(t, e)
+	assert.Equal(t, actual.value, -1)
+
+	testObject.AddInTail(Node{nil, nil, 1})
+	actualNodes := testObject.FindAll(1)
+	assert.Equal(t, len(actualNodes), 2)
+	testObject.Clean()
+	assert.Equal(t, testObject.Count(), 0)
+}
