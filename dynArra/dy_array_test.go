d4447cef8d520e5b6467671c25c50e96a4dbabb8
isaya1910 <ibaigereyev@klika-tech.com>
2022-07-23 18:57:47 +0600

    dyn array

---
+++ dynArra/dy_array_test.go
@@ -0,0 +1,49 @@
+package dynArra
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDynArray_Remove(t *testing.T) {
+	var testDynArray DynArray[int]
+	testDynArray.Init()
+
+	for i := 0; i < 33; i++ {
+		testDynArray.Append(i)
+	}
+
+	require.Equal(t, 64, testDynArray.capacity)
+
+	testDynArray.Remove(3)
+	testDynArray.Remove(5)
+	testDynArray.Remove(6)
+
+	require.Equal(t, 42, testDynArray.capacity)
+
+}
+
+func TestDynArray_Insert(t *testing.T) {
+	var testDynArray DynArray[int]
+	testDynArray.Init()
+
+	for i := 0; i < 32; i++ {
+		testDynArray.Append(i)
+	}
+
+	actual, err := testDynArray.GetItem(0)
+	require.NoError(t, err)
+
+	require.Equal(t, 0, actual)
+
+	require.Equal(t, 32, testDynArray.capacity)
+
+	err = testDynArray.Insert(44, 3)
+	require.NoError(t, err)
+	actual, err = testDynArray.GetItem(3)
+	require.NoError(t, err)
+	require.Equal(t, 44, actual)
+
+	require.Equal(t, 64, testDynArray.capacity)
+
+}
---
+++ dynArra/dyn_array.go
@@ -0,0 +1,78 @@
+package dynArra
+
+import (
+	"fmt"
+)
+
+type DynArray[T any] struct {
+	count    int
+	capacity int
+	array    []T
+}
+
+func (da *DynArray[T]) Init() {
+	da.count = 0
+	da.MakeArray(16)
+}
+
+func (da *DynArray[T]) MakeArray(sz int) {
+	var arr = make([]T, sz)
+	//  копируем содержимое array в arr ...
+	copy(arr, da.array)
+	da.capacity = sz
+	da.array = arr //
+}
+
+func (da *DynArray[T]) Insert(itm T, index int) error {
+	if index >= len(da.array) || index < 0 {
+		return fmt.Errorf("bad index '%d'", index)
+	}
+	if da.count == da.capacity {
+		da.MakeArray(2 * da.capacity)
+	}
+	for i := da.count - 1; i > index; i-- {
+		da.array[i] = da.array[i-1]
+	}
+	da.array[index] = itm
+	return nil
+}
+
+func (da *DynArray[T]) Remove(index int) error {
+	if index >= len(da.array) || index < 0 {
+		return fmt.Errorf("bad index '%d'", index)
+	}
+	for i := index; i < da.count-1; i++ {
+		da.array[i] = da.array[i+1]
+	}
+	da.count--
+
+	capacityPercent := float32(da.count) / float32(da.capacity)
+	newCapacity := (da.capacity * 2) / 3
+
+	if capacityPercent < 0.5 && newCapacity >= 16 {
+		da.capacity = newCapacity
+		return nil
+	}
+	if capacityPercent < 0.5 && newCapacity < 16 {
+		da.capacity = 1
+		return nil
+	}
+	return nil
+}
+
+func (da *DynArray[T]) Append(itm T) {
+	if da.count == da.capacity {
+		da.MakeArray(2 * da.capacity)
+	}
+	da.array[da.count] = itm
+	da.count++
+}
+
+func (da *DynArray[T]) GetItem(index int) (T, error) {
+	var result T
+	if index >= len(da.array) || index < 0 {
+		return result, fmt.Errorf("bad index '%d'", index)
+	}
+	result = da.array[index]
+	return result, nil
+}
