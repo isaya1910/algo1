ec2652b18adfecdcb81aa5d7d7a2538ccd0668a8
issaiya1910 <issaiya.baigereyev@gymshark.com>
2023-05-27 17:45:32 +0600

    api

---
+++ clear_arch/api/server.go
@@ -0,0 +1,13 @@
+package api
+
+/*
+ 	Api для этого модуля я бы разработал ввиде стандартного REST api, есть http запросы такие как
+	GET, POST, PUT, для клиентов нашего модуля достапны запросы.
+
+	GET getRobot
+	POST moveRobot
+	POST setState
+	POST turnRobot
+	POST stopRobot
+
+*/
