374f6a99cc6f9f983afbd77a058f18e9c8527738
issaiya1910 <issaiya.baigereyev@gymshark.com>
2023-05-29 22:57:45 +0600

    stateless

--- clear_arch/robot.go
+++ clear_arch/robot.go
@@ -13,34 +13,48 @@ type Robot interface {
 	Stop()
 }
 
+// RobotLocationRepository /* Реализовал stateless архитектуру через паттерн репозиторий
+/*
+	Убрал все состояния с реализации робота, теперь каждое состояние сохраняется и получается через слой
+	абстракции репозитория. Разницы нет что под имплементацией, это может быть как SQL база данных, или key-value
+	хранилища либо другие структуры данных.
+*/
+type RobotLocationRepository interface {
+	saveRobotLocation(point Point)
+	getRobotLocation() Point
+	saveNewState(state CleaningState)
+	getState() CleaningState
+	getAngle() Angle
+	saveAngle(angle Angle)
+}
+
 type SweeperRobot struct {
-	currentLocation Point
-	state           CleaningState
-	angle           Angle
+	repo RobotLocationRepository
 }
 
 func (s *SweeperRobot) Move(move Move) {
-	angleRads := float64(s.angle) * (math.Pi / 180.0)
-	s.currentLocation = Point{
-		x: s.currentLocation.x + int32(float64(move.Movement.distance)*math.Cos(angleRads)),
-		y: s.currentLocation.y + int32(float64(move.Movement.distance)*math.Sin(angleRads)),
-	}
-	fmt.Println(s.currentLocation)
+	angleRads := float64(s.repo.getAngle()) * (math.Pi / 180.0)
+	repo := s.repo
+	repo.saveRobotLocation(Point{
+		x: repo.getRobotLocation().x + int32(float64(move.Movement.distance)*math.Cos(angleRads)),
+		y: repo.getRobotLocation().y + int32(float64(move.Movement.distance)*math.Sin(angleRads)),
+	})
+	fmt.Println(repo.getRobotLocation())
 }
 
 func (s *SweeperRobot) SetState(state CleaningState) {
-	s.state = state
-	fmt.Println(s.state.Clean())
+	s.repo.saveNewState(state)
+	fmt.Println(s.repo.getState().Clean())
 }
 
 func (s *SweeperRobot) Start() {
-	fmt.Sprintf("Started in location: ", s.currentLocation)
-	s.state.Clean()
+	fmt.Sprintf("Started in location: ", s.repo.getRobotLocation())
+	s.repo.getState().Clean()
 }
 
 func (s *SweeperRobot) Turn(turn Turn) {
-	s.angle += turn.Angle
-	fmt.Println(s.angle)
+	s.repo.saveAngle(turn.Angle)
+	fmt.Println(s.repo.getAngle())
 }
 
 func (s *SweeperRobot) Stop() {
